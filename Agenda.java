829520664e87228b7707a222d584a233cd6f6214
vnmartinez <vn.martinez@hotmail.com>
2022-06-04 23:40:03 -0300

    Criação do projeto

---
+++ Agenda.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.io.File;
+
+
+public class Agenda {
+    public static void main(String[] args) {
+        File arquivo = new File( "../Agenda.txt" );
+    }
+}
